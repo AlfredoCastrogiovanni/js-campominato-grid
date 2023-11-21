98b983069b9e72bb79234023de056a7a829f235b
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-11-21 17:06:22 +0100

    implementazione esercizio con bonus

---
+++ js/index.js
@@ -0,0 +1,33 @@
+
+const wrapper = document.querySelector("main .wrapper");
+const playButton = document.getElementById("playButton");
+
+playButton.addEventListener("click", function() {
+    const squareNumber = parseInt(document.getElementById("difficulty").value);
+    wrapper.innerHTML = "";
+    createSquare(squareNumber);
+});
+
+// ==================== FUNCTIONS ====================
+
+// return a new square
+function createSquare(times) {
+    for(let i=1; i <= times; i++) {
+        const newSquare = document.createElement("div");
+        newSquare.classList.add("square");
+
+        newSquare.style.width = `calc(100% / ${Math.sqrt(times)})`;
+        newSquare.style.height = `calc(100% / ${Math.sqrt(times)})`;
+
+        const squareContent = document.createElement("span");
+            squareContent.textContent = i;
+            newSquare.append(squareContent);
+
+        newSquare.addEventListener("click", function() {
+            this.classList.add("bg-blue");
+            console.log(this.textContent);
+        });
+        
+        wrapper.append(newSquare);
+    }
+}
